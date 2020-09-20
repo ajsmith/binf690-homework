a20798172759cbedd94d5f8dbbbf5e31407e3769
Alex Smith <alex.smith@redhat.com>
2020-09-20 16:52:07 -0400

    Change latex theme to howto/report.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -70,3 +70,6 @@ html_static_path = ['_static']
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
+
+#latex_toplevel_sectioning = None
+latex_theme = 'howto'
