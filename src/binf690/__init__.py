4fea6cd8fbf38e7b487c4f875115657dc9339af7
Alex Smith <alex.smith@redhat.com>
2020-09-20 23:14:50 -0400

    Add module for common numeric methods.

--- src/binf690/__init__.py
+++ src/binf690/__init__.py
@@ -1,8 +1,1 @@
 # Python :)
-
-def machine_epsilon():
-    """Return the relative round-off error of floating point arithmetic."""
-    x = 1.0
-    while 1 < 1.0 + x / 2:
-        x /= 2
-    return x
---
+++ src/binf690/numeric.py
@@ -0,0 +1,10 @@
+"""Utilities for Numeric Methods.
+
+"""
+
+def machine_epsilon():
+    """Return the relative round-off error of floating point arithmetic."""
+    x = 1.0
+    while 1 < 1.0 + x / 2:
+        x /= 2
+    return x
